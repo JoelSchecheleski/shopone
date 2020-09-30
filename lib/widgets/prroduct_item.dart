c5328a51ddc3cad64fa2f3bac7202d505534c509
Joel Schecheleski <joel.jsp@gmail.com>
2020-09-30 16:17:17 -0300

    Adicionado SnackBar ao adicionar produto

--- lib/widgets/prroduct_item.dart
+++ lib/widgets/prroduct_item.dart
@@ -7,8 +7,10 @@ import 'package:shop/providers/cart-provider.dart';
 class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final Product product = Provider.of<Product>(context, listen: false); // fica escutando
-    final Cart cart = Provider.of<Cart>(context, listen: false); // fica escutando
+    final Product product =
+        Provider.of<Product>(context, listen: false); // fica escutando
+    final Cart cart =
+        Provider.of<Cart>(context, listen: false); // fica escutando
 
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
@@ -26,10 +28,12 @@ class ProductItem extends StatelessWidget {
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: Consumer<Product>( // controla somente esse escopo mesmo que o listener esteja falso no pai desse nó.
+          leading: Consumer<Product>(
+            // controla somente esse escopo mesmo que o listener esteja falso no pai desse nó.
 //            child: Text('Nunca muda'),
             builder: (ctx, product, child) => IconButton(
-              icon: Icon(product.isFavorite ? Icons.favorite : Icons.favorite_border),
+              icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
               color: Theme.of(context).accentColor,
               onPressed: () {
                 product.toggleFavorite();
@@ -44,8 +48,21 @@ class ProductItem extends StatelessWidget {
             color: Theme.of(context).accentColor,
             icon: Icon(Icons.shopping_cart),
             onPressed: () {
+              Scaffold.of(context).hideCurrentSnackBar();
+              Scaffold.of(context).showSnackBar(SnackBar(
+                content: Text(
+                  'Produto adicioado com sucesso',
+                  textAlign: TextAlign.center,
+                ),
+                duration: Duration(seconds: 2),
+                action: SnackBarAction(
+                  label: 'DESFAZER',
+                  onPressed: () {
+                    cart.removeSingleItem(product.id);
+                  },
+                ),
+              ));
               cart.addItem(product);
-              print(cart.itemsCount);
             },
           ),
         ),
